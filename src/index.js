8535244c1a29f29f281f0234f1da123a7022e217
Uri <Umkil99@gmail.com>
2026-01-01 17:56:06 +0200

    Improved UI with toasts and messages

--- src/index.js
+++ src/index.js
@@ -1,45 +1,60 @@
 import ReactDOM from "react-dom/client";
 import { useState, useEffect } from "react";
 import "./index.css";
+import { Toaster, toast } from "react-hot-toast";
 
 import TaskList from "./taskList";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
+  const [btnMessage, setbtnMessage] = useState("Loading...");
 
   const API_URL =
     process.env.REACT_APP_RENDER_API_URL || "http://localhost:5000";
 
   useEffect(() => {
-    fetch(`${API_URL}/tasks`)
-      .then((response) => response.json())
-      .then((data) => setTasks(data))
-      .catch((error) => console.error("Error fetching tasks:", error));
+    const fetchTasks = async () => {
+      try {
+        setbtnMessage("Loading...");
+        const response = await fetch(`${API_URL}/tasks`);
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        toast.error(error.message);
+      } finally {
+        setbtnMessage("Add Task");
+      }
+    };
+    fetchTasks();
   }, []);
 
-  const handleClick = () => {
+  const handleClick = async () => {
     if (input.trim() === "") return;
 
-    fetch(`${API_URL}/tasks`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ task: input }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Network response was not ok.");
-        }
-      })
-      .then((res) => {
-        setTasks([...tasks, res.task]);
-        setInput("");
-      })
-      .catch((error) => console.error("Error adding task:", error));
+    setbtnMessage("Adding...");
+
+    try {
+      const response = await fetch(`${API_URL}/tasks`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task: input }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const res = await response.json();
+      setTasks([...tasks, res.task]);
+      setInput("");
+    } catch (error) {
+      console.error("Error adding task:", error);
+      toast.error(error.message);
+    } finally {
+      setbtnMessage("Add Task");
+    }
   };
 
   const handleChange = (e) => {
@@ -51,55 +66,66 @@ const App = () => {
     setInput("");
   };
 
-  const handleDelete = (task) => {
-    fetch(`${API_URL}/tasks/${task._id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Network response was not ok.");
-        }
-      })
-      .then((data) => {
-        const newTasks = tasks.filter((t) => t._id !== data._id);
-        setTasks(newTasks);
-      })
-      .catch((error) => console.error("Error deleting task:", error));
+  const handleDelete = async (task) => {
+    setbtnMessage("Deleting...");
+
+    try {
+      const response = await fetch(`${API_URL}/tasks/${task._id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const data = await response.json();
+      const newTasks = tasks.filter((t) => t._id !== data._id);
+      setTasks(newTasks);
+    } catch (error) {
+      console.error("Error deleting task:", error);
+      toast.error(error.message);
+    } finally {
+      setbtnMessage("Add Task");
+    }
   };
 
-  const handleToggleComplete = (task) => {
-    fetch(`${API_URL}/tasks/${task._id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ isCompleted: !task.isCompleted }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Network response was not ok.");
+  const handleToggleComplete = async (task) => {
+    setbtnMessage("Updating...");
+
+    try {
+      const response = await fetch(`${API_URL}/tasks/${task._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ isCompleted: !task.isCompleted }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const res = await response.json();
+      tasks.map((t) => {
+        if (t._id === res.task._id) {
+          t.isCompleted = res.task.isCompleted;
+          setTasks([...tasks]);
         }
-      })
-      .then((res) => {
-        tasks.map((t) => {
-          if (t._id === res.task._id) {
-            t.isCompleted = res.task.isCompleted;
-            setTasks([...tasks]);
-          }
-        });
-      })
-      .catch((error) => console.error("Error editig task state:", error));
+      });
+    } catch (error) {
+      console.error("Error editing task state:", error);
+      toast.error(error.message);
+    } finally {
+      setbtnMessage("Add Task");
+    }
   };
+
+  const isBtnDisabled = btnMessage !== "Add Task";
 
   return (
     <div className="min-h-screen bg-blue-100 flex items-center justify-center py-10">
+      <div>
+        <Toaster />
+      </div>
       <div className="bg-white p-8 rounded-xl shadow-xl w-full max-w-md">
         <h1 className="text-3xl font-bold text-blue-800 mb-6 text-center">
           Task Manager
@@ -117,9 +143,14 @@ const App = () => {
           ></input>
           <button
             onClick={handleClick}
-            className="w-2/3 sm:w-auto bg-blue-600 text-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-700 transition duration-200"
+            disabled={isBtnDisabled}
+            className={`w-2/3 sm:w-auto ${
+              isBtnDisabled ? "bg-blue-300" : "bg-blue-600"
+            } text-white px-6 py-2 rounded-lg font-semibold ${
+              isBtnDisabled ? "" : "hover:bg-blue-700"
+            } transition duration-200`}
           >
-            Add Task
+            {btnMessage}
           </button>
         </form>
         <TaskList
