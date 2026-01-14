fec1eff1e4b933be84436fa0fd1d1884b6156ec0
Uri <Umkil99@gmail.com>
2026-01-14 11:38:50 +0200

    small navbar improvement to prevent rerendering

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,11 @@
 import ServerStatus from "./serverStatus";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const location = useLocation();
-  const path = location.pathname;
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
-  const isAuthPage = path === "/login" || path === "/register";
+  const isAuthPage = pathname === "/login" || pathname === "/register";
 
   return (
     <nav className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-10">
@@ -21,7 +21,7 @@ const Navbar = () => {
           <button
             onClick={() => {
               localStorage.removeItem("token");
-              window.location.href = "/login";
+              navigate("/login");
             }}
             className={`text-sm font-medium text-gray-500 hover:text-red-600 transition-colors duration-200 flex items-center gap-2 ml-14 ${
               isAuthPage ? "invisible" : ""
